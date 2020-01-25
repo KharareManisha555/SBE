c20286190e5447349d9aa922cfceeab080cf61fd
Miss. Kharare <khararemanisha555@gmail.com>
2020-01-26 00:56:32 +0530

    Manisha added changes

--- website/src/Sidebar/Sidebar.js
+++ website/src/Sidebar/Sidebar.js
@@ -42,6 +42,7 @@ class Sidebar extends Component {
                             );
                         })
                     }
+                    <meta name="keywords" content="Shri Balaji Electricals, Dealers in all types of Insulating Materials, Electric PVC Tape, Kraft Paper, Nomex Insulation Paper, Insulated Fiberglass Sleevings, Cotton Tape, Garware Polyester Film, Insulation Varnish, Rubber Compound, Fibre Glass Products, Soldering Wires and Stics, Super Enamelled Copper Wires, Electronic, Steel Alloy Wire, GDR Presspahn Paper, Black Leatheroid Paper - Aspha make,  Undyed Leatheroid Paper, Khatema Kraft Paper, Heat Shrinkable Sleevs, Dr Beck - Elmo 1 A, Jyoti Insulating Varnish, Red Polyester Tape, White Polyester Tape, Industrial Fibreglass Cables, SBE, GenSurge Inc., shribalajielectricals.in, http://shribalajielectricals.in/, www.sribalajielectricals.net"></meta>
                     <meta name="description" content="Shri Balaji Electricals" />
                 </Helmet>
             </div>
